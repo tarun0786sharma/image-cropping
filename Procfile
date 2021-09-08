b85c87f85e803c00ff7801fd6df8b4caaf8803af
Tarun <tarun0786sharma@gmail.com>
2021-09-08 17:47:48 +0530

    whitenoise configurations done

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn tarun_sharma.wsgi
--- tarun_sharma/settings.py
+++ tarun_sharma/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ymuon3ga^f@*x4z)xe3*j)2(3lquxv%6y7zwov95py4_yd!#+y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['damp-scrubland-60306.herokuapp.com', '127.0.0.1']
 
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
